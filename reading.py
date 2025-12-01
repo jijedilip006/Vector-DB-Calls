0bdf329ec287b4e1f757a600a6b544e76040dca9
Jijendran1010 <jijedilip007@gmail.com>
2025-12-01 12:19:41 +0800

    Create a file to handle the data and aquire it using queries

---
+++ reading.py
@@ -0,0 +1,34 @@
+import os
+from pinecone import Pinecone,SearchQuery,SearchRerank
+
+api_key=os.getenv("PINECONE_API_KEY")
+
+pc = Pinecone(api_key="pcsk_6g3cAn_T7qTFzj7v1BWKHKVSEceNtZrbM7cjRuUjHXyYdSh9hCum8iqAKd1T1gTZpzQpPs")
+
+index_name = "pinecone-client-testing"
+
+dense_index = pc.Index(name=index_name)
+
+#Displays the details of the index
+# stats = dense_index.describe_index_stats()
+# print(stats)
+
+
+def print_results(search_results):
+    for hit in search_results['result']['hits']:
+        print(f"id: {hit['_id']:<5} | score: {round(hit['_score'], 3):<5} | category: {hit['fields']['category']:<10} | text: {hit['fields']['chunk_text']:<50}")
+
+
+# Define the query
+query = "Famous historical structures and monuments"
+
+# Search the dense index
+results = dense_index.search(
+    namespace="testing-namespace",
+    query=SearchQuery(
+        top_k=10,
+        inputs={'text': query}
+    )
+)
+
+print_results(results)
