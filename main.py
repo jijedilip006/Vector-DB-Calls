ae5a304c7159918712ae86dae5e9b7bdd450ed6e
Jijendran1010 <jijedilip007@gmail.com>
2025-12-01 11:43:21 +0800

    create an index to use

--- main.py
+++ main.py
@@ -5,3 +5,13 @@ api_key=os.getenv("PINECONE_API_KEY")
 
 pc = Pinecone(api_key=api_key)
 
+index_name = "Pinecone-client-Testing"
+
+if not pc.has_index(name=index_name):
+    pc.create_index_for_model(
+        name=index_name,
+        cloud='aws',
+        region='us-east-1',
+        embed={"model":"llama-text-embed-v2","field_map":{"text":"chunk_text"}}  # type: ignore
+    )
+
